ce7cd63159d8bb3af40ca6e9154931cb9e30fd8e
Susana Isaza <susisaza@gmail.com>
2016-12-06 17:16:15 -0500

    set up server.js file

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const isDev = !('NODE_ENV' in process.env) && require('dotenv').config() && true;
+
+const app = express();
+const PORT = process.argv[2] || process.env.PORT || 3009;
+
+app.use(logger(isDev ? 'dev' : 'common'));
+
+app.use(bodyParser.json());
+
+// Set up routes:
+// app.use('/')
+
+app.use((err, req, res, next) => {
+  console.error(err, next);
+  res.status(500).send('Something is broken!');
+});
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.listen(PORT);
