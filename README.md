598437cd9accc4e870859be275c2ddab351ec49a
Susana Isaza <susisaza@gmail.com>
2016-12-14 16:19:20 -0500

    start final edits

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # ShareMyNotes
+
+## **Concept:** 
+The app is a tool for students and teachers to centralize and share notes. It is a way for students to take notes during class, and for other students to have access to them. It solves a problem for students who miss a day of class or are not able to take notes during class. It also provides a controlled platform for teachers to review notes their students are basing their homework on, in case there are any errors or specific topics that are not being understood correctly. 
+
+## **Technologies:**
+1. React.js and Draft.js: I want to build the app with Draft.js in React. It is a framework built by Facebook for React that lets users set up a customizable text editor in the browser. It is possible to save the information in JSON format to be stored in a database. 
+2. Node and express server.
+3. PSQL: the app will have user authentication and tables for Cohorts, Topics and Notes. The database needs to have a set structure.
+
+## **User Stories:**
+1. Teachers: teachers have the ability as admins to create a Cohort and set up a list of Topics for that Cohort. They can also create Notes under a specific topic for students to see. They can access all notes from all topics to review them and edit them.
+
+2. Students: students can log into their Cohort and take notes on the app during class. They can save the notes and other students from that same Cohort will be able to see those notes as well. Students can also search through the Topics in their Cohort and look over other notes. 
+
+## **Wireframe:**
+![screen shot 2016-12-06 at 10 59 12 pm](https://git.generalassemb.ly/storage/user/45/files/ac97e336-bc07-11e6-9b57-191d6989de7c)
+
+
+## **Timetable:**
+- Dec-7: planning db setup (schema) & mock test Draft.js 
+- Dec-8: set up webpack & server (basic skeleton)
+- Dec-9: db, set up users & specific routes
+- Dec-10: set up Draft.js
+- Dec-11: Notes CRUD 
+- Dec-12: Notes CRUD functional
+- Dec-13: Test functionality
+- Dec-14: debugging & styling
+
+## **Key Challenges:**
+- Using a different framework (Draft.js) with React.js
+- Setting up the Schema correctly with multiple cross-reference tables
+
+## **ERD:**
+![img_6788](https://git.generalassemb.ly/storage/user/45/files/d60945d0-bbcf-11e6-864a-b63477055f4f)
--- src/components/DisplayNewNote/DisplayNewNote.jsx
+++ src/components/DisplayNewNote/DisplayNewNote.jsx
@@ -2,27 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import DisplayNewNoteCss from './DisplayNewNoteCss.css'
 import { Editor, EditorState, RichUtils, convertFromRaw, Entity, convertToRaw, ContentState, DefaultDraftBlockRenderMap, Modifier } from 'draft-js'
-import type {RawDraftContentBlock} from 'RawDraftContentBlock';
-import type {RawDraftEntity} from 'RawDraftEntity';
+// import type {RawDraftContentBlock} from 'RawDraftContentBlock';
+// import type {RawDraftEntity} from 'RawDraftEntity';
 
-// export type RawDraftContentState = {
-//   blocks: Array<RawDraftContentBlock>,
-//   entityMap: {[key: string]: RawDraftEntity},
-// };
 const divStyle = {
   color: 'red',
   fontWeight: 'bold',
   fontSize: '20px',
 };
 
-// export default React.createClass({
 export default class DisplayNewNote extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       editorState: EditorState.createEmpty(),
-      // contentState: ContentState,
-      // new_note_id: 0
       title: 'None'
     };
     // console.log(this.state.editorState)
@@ -31,11 +24,7 @@ export default class DisplayNewNote extends React.Component {
   onChange(editorState) {
     this.setState({
       editorState,
-      // editor_note: this.state.editorState.toJS().currentContent.blockMap,
-      // editor_note: this.state.editorState
     });
-    // console.log('basic editorState: ', this.state.editorState);
-    // console.log('editor_note is: ', this.state.editor_note);
   }
 
   addTitle(e) {
@@ -45,11 +34,7 @@ export default class DisplayNewNote extends React.Component {
   }
 
   saveEditorNote(e){
-    // this.props.saveNewNote(editor_note);
-    // console.log('clicked save note');
-    // console.log('clicked SAVE note: ', this.state.editor_note);
     const thisraw = convertToRaw(this.state.editorState.getCurrentContent());
-    // const toJSData = this.state.editorState.toJS();
     fetch(`/api/cohort/newnote`, {
       headers: {
         'Content-Type': 'application/json'
@@ -57,26 +42,17 @@ export default class DisplayNewNote extends React.Component {
       method: 'POST',
       body: JSON.stringify({
         content: thisraw,
-        // content: this.state.editorState.toJS(),
         topic_id: this.props.topic_id,
         title: this.state.title
       })
     })
-    .then(this.setState({
-        // editorState: EditorState.createEmpty(),
-    }))
-    // .then(this.getAllGardens())
-    // .throw(alert('no id!'))
     .catch((err) => {
       console.log(err);
     });
     // console.log('clicked SAVE note: ', this.state.editorState)
   }
 
-
-
-
-  // ========= TEXT STYLING BUTTONS ==========
+  // ========= INLINE TEXT STYLING BUTTON FUNCTIONS ==========
   makeBold() {
     this.onChange(RichUtils.toggleInlineStyle(
       this.state.editorState,
@@ -105,46 +81,19 @@ export default class DisplayNewNote extends React.Component {
     ));
   }
 
-
-
   render() {
     const toJSData = this.state.editorState.toJS();
-    const newJS = JSON.stringify(toJSData);
-    // console.log('toJS() editorState full is === ', newJS);
+    console.log('EditorState ==> ', toJSData);
 
-    // const thisraw = convertToRaw(this.state.editorState.getCurrentContent());
-    // console.log('TO raw conversion ', thisraw);
+    const rawJS = JSON.stringify(toJSData);
+    console.log('EditorState Raw ==> ', rawJS);
 
-    // const fromRaw = convertFromRaw(this.state.editorState.getCurrentContent());
-    // console.log('FROM raw conversion ', fromRaw);
-
-    const jsObject = JSON.parse(newJS);
-    // console.log('jsObject ', jsObject);
-    // const contentState = new ContentState(jsObject);
-
-    // console.log('content state ', contentState);
-    // console.log('contentState ', contentState);
-
-    // const toRawData = this.state.editorState.convertToRaw();
-    // console.log('toRAW() editorState full is === ', toRawData);
-
-    // const editor_noteS = this.state.editor_note;
-    // console.log('editorNOTE is === ', editor_noteS);
-    // console.log('Current Content each block ==== ', current.currentContent.blockMap);
-    // const arr = [];
-    // arr.push(current.currentContent.blockMap)
-    // console.log(arr)
-    // const raw = convertToRaw(this.state.editorState.getCurrentContent());
-    // console.log(this.state.editorState.getCurrentContent());
-    // const jsonedRaw = JSON.stringify(raw);
-    // console.log(this.state.editorState);
-    // console.log(jsonedRaw);
+    const thisraw = convertToRaw(this.state.editorState.getCurrentContent());
+    console.log('ContentState ==> ', thisraw);
 
     return(
       <div className="newNoteContainer">
-        {/*<button onClick={this.props.saveEditorNote.bind(this)}>SAVE!</button>*/}
         <h5 id="newNoteHeader">Create New Note</h5><hr />
-
         <div className="wrapperHeaderNewNote">
           <form>
             <label className="newNoteTopic">Title:
@@ -156,22 +105,14 @@ export default class DisplayNewNote extends React.Component {
             </div>
           </form>
         </div>
-        {/*<h2>{this.props.params.repoName}</h2>*/}
         <div className="stylebuttons">
           <button onClick={() => {this.makeItalic();}}><i>I</i></button>
           <button onClick={() => {this.makeUnderline();}}><u>U</u></button>
           <button onClick={() => {this.makeBold();}}><b>B</b></button>
           <button onClick={() => {this.makeStrikeThrough();}}><em>-</em></button>
-
-
         </div>
         <div className="displayNewNoteContent">
-          {/*<div className="editorContainer" style={{ backgroundColor: 'rgb(254,250,180)', width: '99.2%', border: '1px solid rgba(0, 0, 0, 0.5)' }}>*/}
-            <div
-              className="editorContainer"
-              style={{ fontFamily: 'calibri', backgroundImage: 'url("http://savin-it.com/images/2016/02/09/texture-lines-stripes-field-notebook-notebook-yellow-leaf-page-background-lignes-rayure-carnet-jaune-feuille-page.th.jpg")', width: '99.2%', border: '1px solid rgba(0, 0, 0, 0.5)' }
-            }>
-
+          <div className="editorContainer" style={{ fontFamily: 'calibri', backgroundImage: 'url("http://savin-it.com/images/2016/02/09/texture-lines-stripes-field-notebook-notebook-yellow-leaf-page-background-lignes-rayure-carnet-jaune-feuille-page.th.jpg")', width: '99.2%', border: '1px solid rgba(0, 0, 0, 0.5)' }}>
             <Editor
               className="editorBox"
               onChange={(editorState) => { this.onChange(editorState) }}
@@ -181,7 +122,7 @@ export default class DisplayNewNote extends React.Component {
           </div>
         </div>
         <div>
-          {/*{jsonedRaw}*/}
+        {/*{jsonedRaw}*/}
         </div>
       </div>
     )
